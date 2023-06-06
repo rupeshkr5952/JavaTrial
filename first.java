705edd64bde3ae1365c69fcb71a2ea02ea74c826
rupeshkr5952 <rupeshkr5952@gmail.com>
2023-06-07 00:41:01 +0530

    first command

---
+++ first.java
@@ -0,0 +1,12 @@
+class first{
+  public static void main(String data[]){
+    String num = "Rupesh";
+    long a =1263468778889765469l;
+    long b = 2097675976465764678l;
+    char r = 'k';
+
+    System.out.println(num + " \n"+ a+b);
+    System.out.println(r);
+  }
+}
+
